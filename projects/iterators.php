f3fd3a36ceb03b4a29313cfc81bd5982e8fdb561
marcelarie <googlillo@gmail.com>
2021-01-28 17:15:52 +0100

    Add iterators

---
+++ projects/iterators.php
@@ -0,0 +1,27 @@
+<?php
+# for
+for ($vari = 0; $vari <=5; $vari++) {
+    echo $vari;
+};
+
+#foreach
+$abcd = array('a', 'b','c','d');
+
+foreach($abcd as $value ) {
+    echo $value;
+};
+
+#while 
+$i = 1;
+while($i < 10 ) {
+    echo $i++;
+    
+}
+
+#dowhile
+$i = 2;
+do {
+    echo $i;
+} while ($i < 5);
+
+?>
