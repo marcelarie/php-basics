a270c879a0f302222a2aec4c5788f4d715144ee0
marcelarie <googlillo@gmail.com>
2021-01-28 17:48:15 +0100

    Add operators

---
+++ projects/operators.php
@@ -0,0 +1,23 @@
+<?php
+// Create an example of use for arithmetic operators: +, -, *, /, and%
+// Create a usage example for comparison operators: ==,!=, <,>, <=,>=
+// Create an example of use for logical operators: &&, And; ||, Or; ! (NOT); Xor
+
+echo var_dump(1 + 2);
+echo var_dump(1 - 2);
+echo var_dump(1 * 2);
+echo var_dump(1 / 2);
+echo var_dump(1 % 2);
+echo var_dump(1 % 2);
+echo var_dump(1 == 2);
+echo var_dump(1 != 2);
+echo var_dump(1 < 2);
+echo var_dump(1 > 2);
+echo var_dump(1 <= 2);
+echo var_dump(1 >= 2);
+echo var_dump(1 && 2);
+echo var_dump(1 || 2);
+echo var_dump(!1);
+echo var_dump(1 Xor 2);
+
+?>
